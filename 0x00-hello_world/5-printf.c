cd708f24662a691e25b51223fde34878ff86a28d
Amarachioma <amarachukwuchioma1217@gmail.com>
2022-12-08 18:04:01 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
- /**
+/**
  * main - print a string
  * Description: use printf
  * Return: 0
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-	        printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
-	        return (0);
+	 printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	 return (0);
 }
