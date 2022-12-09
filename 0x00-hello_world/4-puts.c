8cfaf6fc5b90e937d5ce45608ea7432e7b6d735c
Amarachioma <amarachukwuchioma1217@gmail.com>
2022-12-08 16:00:29 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,52 @@
+#!/bin/bash
+include <stdio.h>
+
+/**
+ *
+ *  * main - print a line of code using put
+ *
+ *   *
+ *
+ *    * Return: 0
+ *
+ *    */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
