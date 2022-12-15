ee47f6afddaa425066f892bb34a4950f6dd4ae60
Amarachioma <amarachukwuchioma1217@gmail.com>
2022-12-14 19:46:44 -0800

    my sevond commt

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,18 +1,6 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	times_table();
-	return (0);
-}
-#include "main.h"
-
-/**
  * add - adds two integers and returns the result
  * @a: int to be added to b
  * @b: int to be added to a
--- 0x02-functions_nested_loops/10-main.c
+++ 0x02-functions_nested_loops/10-main.c
@@ -9,6 +9,8 @@
 int main(void)
 {
 	int n;
+
 	n = add(89, 9);
+	printf("%d\n", n);;
 	return (0);
 }
