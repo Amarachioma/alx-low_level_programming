3fa7e30d8e282e7b49e7fc943fc9f66080aca8e8
Amarachioma <amarachukwuchioma1217@gmail.com>
2023-01-12 20:48:30 -0800

    update

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -7,9 +7,12 @@
 */
 void *malloc_checked(unsigned int b)
 {
-void *ptr;
-ptr = malloc(b);
-if (ptr == NULL)
-exit(98);
-return (ptr);
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
 }
